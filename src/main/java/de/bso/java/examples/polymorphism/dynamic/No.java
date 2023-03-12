4fc478ee57505e9f5ff960096e11ac183617b1d6
fdai2003 <fdai2003@gmail.com>
2023-03-12 17:03:44 +0100

    unnecessary code removed

--- src/main/java/de/bso/java/examples/polymorphism/dynamic/No.java
+++
@@ -1,9 +0,0 @@
-package de.bso.java.examples.polymorphism.dynamic;
-
-public class No extends Yes {
-
-    @Override
-    public void sayYes() {
-        System.out.println("No");
-    }
-}
--- src/main/java/de/bso/java/examples/polymorphism/dynamic/Yes.java
+++
@@ -1,8 +0,0 @@
-package de.bso.java.examples.polymorphism.dynamic;
-
-public class Yes {
-
-    public void sayYes() {
-        System.out.println("Yes");
-    }
-}
